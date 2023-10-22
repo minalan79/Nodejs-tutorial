05f7fb37b8a32c1a21b92703879db6a2bef2e022
Minalan <94749642+minalan79@users.noreply.github.com>
2023-10-22 14:01:10 +0000

    Initial commit

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
+// const http = require('http');
+
+// const server = http.createServer((req, res)=>{
+//     res.end("hello");
+//     if(req.url === '/') {
+//         res.end('Home Page');
+//     } else if (req.url === '/about') {
+//         res.end('About Page');
+//     } else {
+//         res.end('Error');
+//     }
+// });
+
+// server.listen(5000, ()=>{
+//     console.log('Server is listening on port 5000...')
+// })
+
+// const {readFile} = require('fs');
+
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//         readFile(path, 'utf8', (err, data) => {
+//             if(err) {
+//                 reject(err)
+//             } else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/subfolder/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))
+
+// const start = async() => {
+//     try {
+//         const first = await getText('./content/subfolder/first.txt')
+//         console.log(first)
+//     } catch (error) {
+//         console.log(err)
+//     }
+// }
+
+// start()
 
 
-// const names = require('./4-name');
-// const sayHi = require('./5-utils');
-
-
-
-// console.log (names);
-// console.log(sayHi);
-// sayHi(names.John);
-// sayHi(names.Seetha);
-
-
-const os = require('os')
-
-
-const user = os.userInfo();
-console.log(user);
-
-console.log(`The system uptime is ${os.uptime()/60/60} minutes`);
+let promise = new Promise((resolve, reject) => {
+    //executor
+});
